6cde61c5f0c2739c2f5e8f26a4f90909419ab84a
Vivek Dholakiya <97219153+vivekdholakiya@users.noreply.github.com>
2022-08-24 12:22:56 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>login form</title>
+    <link rel="stylesheet" type="text/css" href="loginstyle.css">
+</head>
+<body>
+<div class="login-box">
+    <h1>
+        Login
+    </h1> 
+    <form>
+        <div class="user-box">
+            <input type="text" name="" required>
+            <label>Username</label>
+        </div>
+        <div class="user-box">
+            <input type="password" name="" required>
+            <label>Password</label>
+        </div>
+        <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Submit
+        </a>
+    </form>   
+</div>
+</body>
+</html>
